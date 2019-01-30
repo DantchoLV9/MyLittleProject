d985182b5aca0cd06eb6fc662e45c46bda0e310b
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2019-01-30 20:54:44 +0000

    Removed Unused Code
    The ownprofile blade template had code for an alert of type danger. This was used before the avatar image form validation was added. The unused code in the
    ownprofile.blade.php was removed.

--- resources/views/ownprofile.blade.php
+++ resources/views/ownprofile.blade.php
@@ -15,15 +15,6 @@
         </div>
       @endif
 
-      @if (session('error'))
-        <div class="alert alert-danger alert-dismissible fade show mt-3" role="alert">
-          {{ session('error') }}
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-      @endif
-
       <div class="card border-secondary bg-light mt-3">
         <div class="card-header">
           My Profile
