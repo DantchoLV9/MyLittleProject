979cab3d8877d783c861280420f43dede2b03994
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2019-02-01 19:31:06 +0000

    Added general settings page in the dashboard
    Added a controler, view and route for generail settings page.
    Changed sidenavigationbar in the dashboard.

--- resources/views/dashboard/layouts/sidenavigationbar.blade.php
+++ resources/views/dashboard/layouts/sidenavigationbar.blade.php
@@ -7,63 +7,24 @@
         </div>
     
         <ul class="list-unstyled components">
-          <!-- <p class="sidebar-subheader">Subheading</p> -->
-          <li class="active">
-            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-              <i class="fas fa-home"></i>
+          <li>
+            <a href="{{ route('dashboard') }}">
+              <i class="fas fa-tachometer-alt"></i>
               Home
             </a>
-            <ul class="collapse list-unstyled" id="homeSubmenu">
+          </li>
+          <li>
+            <a href="#settingsSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+              <i class="fas fa-sliders-h"></i>
+              Settings
+            </a>
+            <ul class="collapse list-unstyled" id="settingsSubmenu">
               <li>
-                <a href="#">Home 1</a>
-              </li>
-              <li>
-                <a href="#">Home 2</a>
-              </li>
-              <li>
-                <a href="#">Home 3</a>
+                <a href="{{ route('settingsGeneral') }}">General Settings</a>
               </li>
             </ul>
           </li>
-          <li>
-            <a href="#">
-              <i class="fas fa-briefcase"></i>
-              About
-            </a>
-            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-              <i class="fas fa-copy"></i>
-              Pages
-            </a>
-            <ul class="collapse list-unstyled" id="pageSubmenu">
-              <li>
-                <a href="#">Page 1</a>
-              </li>
-              <li>
-                <a href="#">Page 2</a>
-              </li>
-              <li>
-                <a href="#">Page 3</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fas fa-image"></i>
-              Portfolio
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fas fa-question"></i>
-              FAQ
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fas fa-paper-plane"></i>
-              Contact
-            </a>
-          </li>
+          
         </ul>
       </nav>
 
