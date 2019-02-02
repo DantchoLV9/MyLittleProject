3405761f4792947adb09e7887dfe03bead7ca6df
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2019-02-02 14:20:35 +0000

    Started Work On General Website Settings Page
    Added a blade template for the general website settings page. Also added a model, migration and a controller.

--- resources/views/dashboard/settings/general.blade.php
+++ resources/views/dashboard/settings/general.blade.php
@@ -11,44 +11,17 @@
             General Settings
           </div>
           <div class="card-body">
-            <h5 class="card-title">Title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis cursus ipsum sed purus imperdiet
-              volutpat ac a velit. Ut semper blandit mauris, at laoreet felis dictum laoreet. Aenean consequat magna non orci
-              vestibulum ultrices vitae ac turpis. In fringilla est semper elementum pulvinar. Fusce venenatis, lectus sit amet
-              fermentum accumsan, eros sem rhoncus ipsum, sed finibus mi augue id orci. Vestibulum ac ex at ligula dignissim
-              viverra. Nullam vitae velit placerat, consectetur massa sed, commodo lectus. Donec gravida elementum finibus.
-              Duis pretium urna ut rhoncus tempus. Cras a augue at ante mattis varius ut convallis magna.
-        
-              Sed sodales mauris non ante congue, in tempor sapien posuere. Vivamus at felis vel felis molestie mattis
-              tristique convallis justo. Praesent sed porttitor orci, sed ornare enim. Morbi efficitur iaculis finibus. Donec
-              id dui orci. Ut pharetra sapien nisi, ut cursus neque interdum non. Nam nec hendrerit urna. Nunc vitae metus id
-              ipsum malesuada viverra. Aenean imperdiet felis sed ipsum tristique elementum. Nullam laoreet elit arcu, ornare
-              fringilla risus porta nec. Suspendisse potenti. Integer sodales mattis arcu a faucibus. Mauris sed mauris et mi
-              pretium lobortis. Sed pharetra, mi eget posuere egestas, neque quam commodo lacus, euismod ullamcorper nibh
-              mauris sit amet enim.
-        
-              Ut nisl neque, posuere sit amet posuere eget, lacinia a nisl. Sed commodo rutrum nisi a feugiat. In molestie sit
-              amet risus in vestibulum. Nulla finibus enim eu turpis ullamcorper, eget laoreet turpis faucibus. Donec
-              ullamcorper ante vitae risus ullamcorper scelerisque. Aenean fringilla feugiat urna, ac elementum leo sagittis
-              at. Vestibulum at porta sapien. Fusce velit ante, tristique id tempor vel, auctor id lectus. Interdum et
-              malesuada fames ac ante ipsum primis in faucibus. Vestibulum blandit augue mollis hendrerit dignissim. Nulla
-              consequat eros id eros ultrices, vel hendrerit ex varius. Vivamus ut neque metus. Vivamus a nisi dapibus, mattis
-              eros sed, efficitur lacus. Donec eget eros nec tortor mollis laoreet id eu erat. Sed lobortis, nibh in viverra
-              imperdiet, arcu dui dapibus metus, sed lacinia magna sem sed lorem.
-        
-              Sed quam lorem, lobortis euismod rhoncus ultricies, dapibus eget mi. Nullam sit amet augue nec sem tempor lacinia
-              vitae in nulla. Cras tellus dolor, fermentum a semper sit amet, pharetra convallis enim. Nullam in sem sed lorem
-              rutrum porttitor ac ac leo. Duis auctor ipsum vel libero vehicula euismod. Suspendisse nec auctor risus. Nullam
-              hendrerit dolor dolor, ultricies vulputate turpis ullamcorper sit amet. Proin semper ut mi vel feugiat. Duis non
-              dui ipsum. Phasellus faucibus quis nisi eu egestas.
-        
-              Ut tempor ipsum et neque mattis eleifend. In volutpat sapien tortor, eu porttitor purus ullamcorper vel. Nulla
-              facilisi. Sed non quam risus. Duis vulputate mi at nisl venenatis egestas. Vestibulum ante ipsum primis in
-              faucibus orci luctus et ultrices posuere cubilia Curae; Aenean faucibus massa nec sem porttitor, in lobortis dui
-              bibendum. Ut eget lobortis augue. Nunc et odio bibendum, porta ligula sit amet, maximus elit. Orci varius natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora
-              torquent per conubia nostra, per inceptos himenaeos. Sed in tellus dui. Aenean congue lobortis enim ut
-              pellentesque. Proin eros risus, sollicitudin sit amet ex sed, pharetra pharetra dui.</p>
+            <form>
+              <div class="form-group">
+                <label for="formGroupExampleInput">Website Name</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Website Name">
+              </div>
+              <div class="form-group">
+                <label for="formGroupExampleInput2">Website Address</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Website Address">
+              </div>
+              <input type="submit" class="btn btn-primary" id="formGroupExampleInput2" placeholder="Another input">
+            </form>
           </div>
         </div>
 
